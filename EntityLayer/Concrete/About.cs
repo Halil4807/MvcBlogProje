966904ccb53befff0dac897b979bcfba929c02c1
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-22 23:07:17 +0300

    About sayfasına karakter sınırının eklenmesi

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,13 @@ namespace EntityLayer.Concrete
     {
         [Key]
         public int AboutID { get; set; }
+        [StringLength(1000)]
         public string AboutDetails1 { get; set; }
+        [StringLength(1000)]
         public string AboutDetails2 { get; set; }
+        [StringLength(100)]
         public string AboutImage1 { get; set; }
+        [StringLength(100)]
         public string AboutImage2 { get; set; }
     }
 }
