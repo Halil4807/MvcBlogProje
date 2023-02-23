03dc91863a081ec37b22dc773f8f9102b18479e8
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-23 19:05:18 +0300

    DataAccessLayer EntityFramework eklemesi

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayer
+namespace DataAccessLayer.Concrete
 {
-    public class Class1
+    class Context
     {
     }
 }
