84ce14ae8b13393a63b01c66462b7ea1df2bfe1e
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-24 17:22:41 +0300

    Generic Repository

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace DataAccessLayer.Abstract
         void Insert(Tablo parametre);
         void Delete(Tablo parametre);
         void Update(Tablo parametre);
+        List<Tablo> List(Expression<Func<Tablo, bool>> filter);
     }
 }
--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -1,5 +1,4 @@
 ﻿using DataAccessLayer.Abstract;
-using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,36 +9,42 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete.Repositories
 {
-    public class CategoryRepository : ICategoryDal
+    public class GenericRepository<Tablo> : IRepository<Tablo> where Tablo : class
     {
         Context c = new Context();
-        DbSet<Category> _object;
-        public void Delete(Category parametre)
+        DbSet<Tablo> _object;
+
+        public GenericRepository()
+        {
+            _object = c.Set<Tablo>();
+        }
+
+        public void Delete(Tablo parametre)
         {
             _object.Remove(parametre);
             c.SaveChanges();
             //throw new NotImplementedException();
         }
 
-        public void Insert(Category parametre)
+        public void Insert(Tablo parametre)
         {
             _object.Add(parametre);
             c.SaveChanges();
             //throw new NotImplementedException();
         }
 
-        public List<Category> List()
+        public List<Tablo> List()
         {
             return _object.ToList();
             //throw new NotImplementedException();
         }
 
-        public List<Category> List(Expression<Func<Category, bool>> filter)
+        public List<Tablo> List(Expression<Func<Tablo, bool>> filter)
         {
             throw new NotImplementedException();
         }
 
-        public void Update(Category parametre)
+        public void Update(Tablo parametre)
         {
             //Değişikler direk olarak kaydedilir. Çünkü güncellemeden önce değişiklikler zaten yansıtılıyor.
             c.SaveChanges();
