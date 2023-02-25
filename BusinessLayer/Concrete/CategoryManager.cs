a5aa9920e08289f844d265b30cbccbaf97d086fa
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-25 23:48:41 +0300

    Update CategoryManager

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -20,7 +20,7 @@ namespace BusinessLayer.Concrete
         {
             if (par.CategoryName == "" || par.CategoryDescription == "" || par.CategoryName.Length <= 3 || par.CategoryName.Length > 50)
             {
-                //Hata Mesajı buaraya yazılacak
+                //Hata Mesajı buaraya yazılacak!!
             }
             else
             {
