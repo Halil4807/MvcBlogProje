6629e820f800b4cce17f6fa83cd940dec91bf04a
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-02 03:56:13 +0300

    yazar edit

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -15,9 +15,7 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçemezsiniz!!");
             RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş geçemezsiniz!!");
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Yazar hakkında boş geçemezsiniz!!"); 
-            //RuleFor(x => x.WriterAbout).NotEmpty().When(x=>x.WriterAbout.ToUpper().Contains("A")).WithMessage("Yazar hakkında boş geçemezsiniz!!"); 
-            //Alternatif Çözüm
-            //RuleFor(x => x.WriterAbout).Must(x => x != null && x.ToUpper().Contains("A")).WithMessage("Hakkında kısmında en az bir a harfi içermelidir");
+            RuleFor(x => x.WriterAbout).Must(x => x != null && x.ToUpper().Contains("A")).WithMessage("Hakkında kısmında en az bir A harfi içermelidir");
             RuleFor(x => x.WriterImage).NotEmpty().WithMessage("Yazar resmi boş geçemezsiniz!!");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar maili boş geçemezsiniz!!");
             RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Yazar mail formaya uygun değil!!");
