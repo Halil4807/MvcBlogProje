7f2f30cb7a724cfca421cfff271867752718203d
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-09 12:12:17 +0300

    HeadingController oluşurulması ve kod yazılabilir hale getirilmesi

---
+++ Controllers/HeadingController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlogProje.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DataAccessLayer/EntityFramework/EfHeadingDal.cs
+++ DataAccessLayer/EntityFramework/EfHeadingDal.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfHeadingDal
+    public class EfHeadingDal:GenericRepository<Heading>,IHeadingDal
     {
     }
 }
