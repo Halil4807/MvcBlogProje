bb46f6d419f35c530496740c33b44bea7ab9785a
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-10 15:00:41 +0300

    Yazarları başlık ekleme alanına dropdown ile listeleyip ekletme

--- Controllers/HeadingController.cs
+++ Controllers/HeadingController.cs
@@ -14,6 +14,7 @@ namespace MvcBlogProje.Controllers
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
             var headingvalue = hm.GetList();
@@ -24,6 +25,8 @@ namespace MvcBlogProje.Controllers
         {
             List<SelectListItem> categorylist = (from x in cm.GetList() select new SelectListItem {Text=x.CategoryName, Value=x.CategoryID.ToString() }).ToList();
             ViewBag.categoriler = categorylist;
+            List<SelectListItem> yazarlarlist = (from x in wm.GetListBL() select new SelectListItem { Text = x.WriterName + " " + x.WriterSurName, Value = x.WriterID.ToString() }).ToList();
+            ViewBag.yazarlar = yazarlarlist;
             return View();
         }
         [HttpPost]
