b0f1c84cba3487dad2f60121c537b845dd09fa98
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-12 14:41:53 +0300

    Heading'e göre cotentlerin listelenmesi

--- Controllers/ContentController.cs
+++ Controllers/ContentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,14 +11,16 @@ namespace MvcBlogProje.Controllers
     public class ContentController : Controller
     {
         // GET: Content
+        ContentManager cm = new ContentManager(new EfContentDal());
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ContentByHeading()
+        public ActionResult ContentByHeading(int id)
         {
-            return View();
+            var contentvalue = cm.GetListByIDBL(id);
+            return View(contentvalue);
         }
     }
 }
