ca5bcf87979c3fcddb7b2a8669499ff775eb3963
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-14 23:38:18 +0300

    About BusinessLayer katmanında tanımlamalarının yapılması

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            this.aboutDal = aboutDal;
+        }
+
+        public void AboutAddBL(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutDeleteBL(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutUpdateBL(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
