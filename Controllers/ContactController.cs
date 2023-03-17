5fff0a9f3f4107dac9fa6966d78f1076be83b22c
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-17 21:18:08 +0300

    ContactController'da Manager'ı çağırma

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,7 @@ namespace MvcBlogProje.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager cm = new ContactManager(new EfContactDal());
         public ActionResult Index()
         {
             return View();
