275b2abc7a77d773cfb4764289b8066c56a3dfdc
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-18 20:17:46 +0300

    BusinessLayer Message Manager oluşturma

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public Message GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageAddBL(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageDeleteBL(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdateBL(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
