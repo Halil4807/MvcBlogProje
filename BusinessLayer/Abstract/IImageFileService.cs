f74653429ae34a816861b1bb0fc1fdd4e5709d36
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-20 02:40:04 +0300

    Galeri oluşturma
    EntityLayer katmanında ImageFile,
    DataAccess katmanında IImageFileDal,EfImageFileDal ve Context(ImageFiles)
    Business katmanında IImageService, ImageFileManager
    UI katmanında Controller ve Index sayfanına templete eklenerek verilerin gösterilmesi

---
+++ BusinessLayer/Abstract/IImageFileService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IImageFileService
+    {
+        List<ImageFile> GetList();
+        void ImageFileAddBL(ImageFile imageFile);
+        ImageFile GetById(int id);
+        void ImageFileDeleteBL(ImageFile imageFile);
+        void ImageFileUpdateBL(ImageFile imageFile);
+    }
+}
---
+++ Controllers/GaleriController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlogProje.Controllers
+{
+    public class GaleriController : Controller
+    {
+        // GET: Galeri
+        ImageFileManager im = new ImageFileManager(new EfImageFileDal());
+        public ActionResult Index()
+        {
+            var imagefilevalue = im.GetList();
+            return View(imagefilevalue);
+        }
+    }
+}
