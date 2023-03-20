685482f09e10963bf5a2aa59d073b709b9101bac
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-20 04:29:53 +0300

    Kategori sayısını hesaplama

--- Controllers/IstatisticController.cs
+++ Controllers/IstatisticController.cs
@@ -15,7 +15,14 @@ namespace MvcBlogProje.Controllers
         WriterManager wm = new WriterManager(new EfWriterDal());
         public ActionResult Index()
         {
+            //List<SelectListItem> categorylist = (from x in cm.GetList() select new SelectListItem { Text = x.CategoryName, Value = x.CategoryID.ToString() }).ToList();
+            //ViewBag.categoriler = categorylist;
+            var categorylist= cm.GetList();
+            ViewBag.categoriler =  categorylist.Count();
+            foreach (var item in categorylist)
+            {
 
+            }
             return View();
         }
     }
