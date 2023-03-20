f0c2aad2f43a68625b7eec9578fbf2cd6ea5c0f6
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-20 03:05:37 +0300

    Login Teması oluşturma
    Katmanlarda admin alanının oluşturulması

---
+++ DataAccessLayer/Migrations/202303192348499_mig_Admin_add_class.cs
@@ -0,0 +1,30 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_Admin_add_class : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        AdminID = c.Int(nullable: false, identity: true),
+                        AdminUserName = c.String(maxLength: 50),
+                        AdminPassword = c.String(maxLength: 50),
+                        AdminRole = c.String(maxLength: 1),
+                        AdminMail = c.String(maxLength: 50),
+                        AdminImage = c.String(maxLength: 250),
+                    })
+                .PrimaryKey(t => t.AdminID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Admins");
+        }
+    }
+}
