c9b36cb5513fbb18816efcc291e94fdc237ba88f
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-20 04:20:03 +0300

    İstatistikleri için sayfanın oluşturulması

---
+++ Controllers/IstatisticController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlogProje.Controllers
+{
+    public class IstatisticController : Controller
+    {
+        // GET: Istatistic
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
