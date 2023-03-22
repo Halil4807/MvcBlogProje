5fa191c01ea8e97d229dbbeca2391517db1c5aac
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-22 03:40:03 +0300

    Wrtier Heading Edit

--- Controllers/WriterPanelController.cs
+++ Controllers/WriterPanelController.cs
@@ -36,7 +36,22 @@ namespace MvcBlogProje.Controllers
         {
             heading.HeadingDate = DateTime.Now;
             heading.WriterID = 4;
+            heading.HeadingStatus = true;
             hm.HeadingAddBL(heading);
+            return RedirectToAction("MyHeading");
+        }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> categorylist = (from x in cm.GetList() select new SelectListItem { Text = x.CategoryName, Value = x.CategoryID.ToString() }).ToList();
+            ViewBag.categoriler = categorylist;
+            var headingvalue = hm.GetById(id);
+            return View(headingvalue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading heading)
+        {
+            hm.HeadingUpdateBL(heading);
             return RedirectToAction("MyHeading");
         }
     }
