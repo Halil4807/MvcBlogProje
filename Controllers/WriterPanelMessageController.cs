1f092a88a2f0c0145daf5917c98b61612af8db63
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-22 05:31:53 +0300

    Message Sayfasının Yenimesaj haricinde yapımı

--- Controllers/WriterPanelMessageController.cs
+++ Controllers/WriterPanelMessageController.cs
@@ -19,5 +19,18 @@ namespace MvcBlogProje.Controllers
             var messagelist = mm.GetListInbox();
             return View(messagelist);
         }
+        public PartialViewResult WriterMailMenuPartial()
+        {
+            var messagelistSend = mm.GetListSendboxNotRead();
+            ViewBag.SendMailCount = messagelistSend.Count();
+            var messagelistInbox = mm.GetListInboxNotRead();
+            ViewBag.InboxMailCount = messagelistInbox.Count();
+            return PartialView();
+        }
+        public ActionResult Sendbox()
+        {
+            var messagelist = mm.GetListSendbox();
+            return View(messagelist);
+        }
     }
 }
