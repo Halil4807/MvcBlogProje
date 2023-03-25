d73b51e40e78feb7f2d3a53c3908977d348ed482
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-03-25 22:57:06 +0300

    Vitrin bölümünün oluşturulması ve Başlıkların listelenmesi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,16 @@ namespace MvcBlogProje.Controllers
 {
     public class HomeController : Controller
     {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Headings()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var headingList = hm.GetList();
+            return View(headingList);
         }
     }
 }
